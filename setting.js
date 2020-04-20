d3d49485be9118b15f90943798eda98cddde5cbe
almogsa <almogsa@post.bgu.ac.il>
2020-04-20 19:32:27 +0300

    continue setting

---
+++ setting.js
@@ -0,0 +1,73 @@
+$("#settingForm").validate({
+    rules: {
+    rightKey: {
+        required: true,
+        minlength: 1,
+        maxlength: 1
+    },
+    leftKey: {
+        required: true,
+        minlength: 1,
+        maxlength: 1
+    },
+    upKey: {
+        required: true,
+        minlength: 1,
+        maxlength: 1
+    },
+    downKey: {
+        required: true,
+        minlength: 1,
+        maxlength: 1
+    },
+    ballsNum: {
+        required: true,
+        range: [50,90]
+    },
+    timeGame: {
+        required: true,
+        min: 60
+    },
+    monstersNum: {
+        required: true,
+        range: [1,4]
+    },
+
+},
+messages: {
+    rightKey: {
+        required: "You must enter a key",
+        minlength:"Right key must be one character",
+        maxlength: "Right key must be one character"
+     },
+     leftKey: {
+        required: "You must enter a key",
+        minlength:"Left key must be one character",
+        maxlength: "Left key must be one character"
+    },
+    upKey: {
+        required: "You must enter a key",
+        minlength:"Up key must be one character",
+        maxlength: "Up key must be one character"
+    },
+    downKey: {
+        required: "You must enter a key",
+        minlength:"Down key must be one character",
+        maxlength: "Down key must be one character"
+    },
+    
+    ballsNum: {
+        required: "You must enter a number",
+        range: "Balls number must be between 60 and 90"
+    },
+    timeGame:{
+        required: "You must enter a number",
+        min: "Time game must be minimum 60 seconds"
+    },
+    monstersNum:{
+        required: "You must enter a number",
+        range: "Monsters number must be between 60 and 90"
+    },
+    
+},   
+});
